7de459bc5501bd878a45140705cb75177fad433c
Methsara Perera <71716579+methsaraperera@users.noreply.github.com>
2024-02-22 16:06:46 -0500

    Add files via upload

--- signup.php
+++ signup.php
@@ -21,24 +21,38 @@
     <div class="navbar-buttons">
         <button class="navbar-button">Home</button>
         
-        <button class="navbar-button">Sign Up</button>
+        <button class="navbar-button">Sign In</button>
     </div>
 </div>
 <div class="full-screen-container">
     <div class="form-container">
-        <h2>Login</h2>
-       
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username">
+        <h2>Sign Up</h2>
+          <form method="POST" class="flex-form" action="php/signup.php" enctype="multipart/form-data">
+          
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="fname">
+
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lname">
+
+            <label for="cunyid">CUNY First ID</label>
+            <input type="number" id="cunyid" name="cunyid">
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
         
             <label for="password">Password</label>
             <input type="password" id="password" name="password">
+
+            <label for="re-password">Re-enter Password</label>
+            <input type="password" id="re-password" name="re-password">
         
-            <a href="index.html"><button type="submit">Log In</button></a>
-        
+            <input type="submit" class="form-button" name="Register" value="Register">
+
+            
+          </form>   
     </div>
 </div>
-
 
 <script src="js/watson.js"></script>
 
