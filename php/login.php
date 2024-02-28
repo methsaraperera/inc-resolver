5b360cc266e8bee448971d5743fd87b280f1e25a
Methsara Perera <71716579+methsaraperera@users.noreply.github.com>
2024-02-28 01:29:41 -0500

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -6,11 +6,11 @@ if (isset($_POST['Login'])) {
     $email = ($_POST['email']);
     $pw = ($_POST['password']);
     $role = ($_POST['role']); // checking for the role of the user
-    if($role == 2){
+    if($role == 2){ // login for role 2 (students)
         if (!empty($email) && !empty($pw)) {
             $sql = mysqli_query($conn, "SELECT * FROM student WHERE email = '{$email}'");
             if (!mysqli_num_rows($sql) > 0) {
-                header("Location: ../login.php?error-non=true"); // user non exist
+                header("Location: ../login.php?stu=true&error-non=true"); // user non exist
                 exit();
             } 
             else {
@@ -20,21 +20,56 @@ if (isset($_POST['Login'])) {
                     $db_pw = $row['password'];
                     $uid = $row['cunyid']; // will be used in future developments as the session key for login
                     if ($pw == $db_pw) {
-                        echo 'success'; // successfully logged in
+                        //echo 'success'; // successfully logged in
+                        header("Location: ../student-dashboard.html"); // invalid password
+                        exit();
                     } else {
-                        header("Location: ../login.php?error-pw=true"); // invalid password
+                        header("Location: ../login.php?stu=true&error-pw=true"); // invalid password
                         exit();
                     }
                 } else {
-                    header("Location: ../login.php?error=true"); // unknown error
+                    header("Location: ../login.php?stu=true&error=true"); // unknown error
                     exit();
                 }
             }
         } else {
-            header("Location: ../login.php?error-inc=true"); // form invalid
+            header("Location: ../login.php?stu=true&error-inc=true"); // form invalid
             exit();
         }
     }
+
+    elseif($role == 1){ // login for role 1 (instructor)
+        if (!empty($email) && !empty($pw)) {
+            $sql = mysqli_query($conn, "SELECT * FROM instructor WHERE email = '{$email}'");
+            if (!mysqli_num_rows($sql) > 0) {
+                header("Location: ../login.php?inc=true&error-non=true"); // user non exist
+                exit();
+            } 
+            else {
+                $search_password = mysqli_query($conn, "SELECT cunyid, password FROM instructor WHERE email = '{$email}'");
+                if (mysqli_num_rows($search_password) > 0) {
+                    $row = mysqli_fetch_assoc($search_password);
+                    $db_pw = $row['password'];
+                    $uid = $row['cunyid']; // will be used in future developments as the session key for login
+                    if ($pw == $db_pw) {
+                        echo 'success'; // successfully logged in
+                    } else {
+                        header("Location: ../login.php?inc=true&error-pw=true"); // invalid password
+                        exit();
+                    }
+                } else {
+                    header("Location: ../login.php?inc=true&error=true"); // unknown error
+                    exit();
+                }
+            }
+        } else {
+            header("Location: ../login.php?inc=true&error-inc=true"); // form invalid
+            exit();
+        }
+    }
+
+
+    // login for role 0 (bursar) goes here
     else{
         echo 'err role inc';
     }
